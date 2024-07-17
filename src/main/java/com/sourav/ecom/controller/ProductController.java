273bf0f3f17c88064fdf45979b9ef88c1c0b7940
Sourav Jyoti Nath <souravjyotinath2022@gmail.com>
2024-07-18 01:10:42 +0530

    image uploading process

--- src/main/java/com/sourav/ecom/controller/ProductController.java
+++ src/main/java/com/sourav/ecom/controller/ProductController.java
@@ -4,8 +4,10 @@ import com.sourav.ecom.model.Product;
 import com.sourav.ecom.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -33,5 +35,25 @@ public class ProductController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile img){
+        try {
+            Product pr = service.addProduct(product, img);
+            return new ResponseEntity<>(pr, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/products/{id}/image")
+    public ResponseEntity<byte[]> getImgById(@PathVariable int id){
+        Product product = service.getOneProduct(id);
+        byte[] img = product.getImgData();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImgType()))
+                .body(img);
     }
 }
